10c24b6db68b68b223f6c7523852b39fac944775
julietraevskaua <3Kinder333@gmail.com>
2019-11-06 15:24:50 +0200

    calculator

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -7,8 +7,12 @@ using namespace std;
 int main()
 {
 	int a, b, sum;
-	cin >> a >> b;
-	sum = a + b;
+	char c;
+	cin >> a >> c >> b;
+	if (c == '+')
+		sum = a + b;
+	if else (c == '-')
+		sum = a - b;
 	cout << sum << endl;
 	return(0);
 }
