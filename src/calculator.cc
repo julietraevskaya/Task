37761741b80313485a07afd60e86a7918977eb19
julietraevskaua <3Kinder333@gmail.com>
2019-11-07 17:44:14 +0200

    calculator

--- src/calculator.cc
+++ src/calculator.cc
@@ -1,15 +1,25 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-	int a, b, sum;
-	char c;
-	cin >> a >> c >> b;
-	if (c == '+')
-		sum = a + b;
-	if  (c == '-')
-		sum = a - b;
-	cout << sum << endl;
+	int j = 1;
+	int sum = 0;
+	if (argc > 1)
+	{
+		for(int i = 0; i <= argc; i++)
+		{
+			if (argv[j] == "+")
+				++argv[j];
+			else
+			{
+				sum = sum + atoi(argv[j]);
+				++argv[j];
+			}
+			
+		}
+		cout << sum << endl;
+	}
 	return(0);
 }
